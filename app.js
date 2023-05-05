51728958e9ae19649548fc2f351a4dd8144f66be
kewal khondekar <kewalkhondekar25@gmail.com>
2023-05-05 19:39:40 +0530

    cors reselved v1

--- app.js
+++ app.js
@@ -3,23 +3,24 @@
 //setup .env
 require('dotenv').config()
 
+//extra security package
+const helmet = require('helmet')
+const cors = require("cors")
+const xss = require('xss-clean')
+
 //importing mongoose module
 const connectDB = require('./database/connect')
 
 
 const express = require('express')
 const app = express()
-app.use(cors())
+//app.use(cors())
+app.use(helmet())
+app.use(xss())
+
 
 //importing router
 const flights = require('./routes/flights')
-
-
-//extra security package
-const helmet = require('helmet')
-const cors = require("cors")
-const xss = require('xss-clean')
-
 
 
 //setup middleware
@@ -27,11 +28,8 @@ app.use(express.json())
 
 
 //implementing router
-app.use('/api/v1/flights', flights)
+app.use(cors('/api/v1/flights', flights))
 
-app.use(helmet())
-
-app.use(xss())
 
 app.get('/', (req, res) => {
     res.send('<h1 style = " background-color:#F5F5F7; color:#7AA874; text-align:center; font-family: Helvetica, Arial; font-weight: 600px; font-size:48px; color:black; margin-top: 250px;">Welcome Onboard!</br><p style="color:grey; font-size:35px"> <a href="#">FlightsAPI</a></p></h1>')
